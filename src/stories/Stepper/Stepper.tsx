59204510056af82c5f8b252f0e844b91c4e394e9
MarcHerasBSC <marc.heras@bsc.es>
2024-12-12 16:56:38 +0100

    Add stepper to Top Bar

---
+++ src/stories/Stepper/Stepper.tsx
@@ -0,0 +1,34 @@
+import { Flex, IconButton, Separator, Text } from '@radix-ui/themes'
+import React from 'react'
+import styles from './Stepper.module.css'
+import { CheckIcon } from '@radix-ui/react-icons'
+
+interface StepType {
+  id: number,
+  name: string
+}
+
+interface PropType {
+  steps: StepType[],
+  currentStep: number
+}
+
+export default function Stepper({steps, currentStep}: PropType) {
+  return (
+    <Flex direction='row' align='center' justify="start" className={styles.stepperBg} pr="4">
+      {steps.map((step, i) => { 
+        return(
+        <Flex key={"step-container-"+i} direction='row'  align='center' flexGrow={i === 0 ? "0" : "1"} flexShrink="1">
+          {i === 0 ? null : <Separator mx="4" size="4"/>}
+          <Flex direction="row" align="center" gap="2" width="fit-content">
+            <IconButton variant={step.id+1 > currentStep ? "outline" : step.id+1 === currentStep ? "solid" : "soft"} radius="full" color="blue" size="3">
+              {step.id+1 < currentStep ? <CheckIcon width={20} height={20}/> : <Text size="3" weight="bold">{step.id+1}</Text>}
+            </IconButton>
+            <Text size="3" weight={step.id+1 === currentStep ? "bold" : "regular"}>{step.name}</Text>
+          </Flex>
+        </Flex>
+        )})
+      }
+    </Flex>
+  )
+}
--- src/stories/TopBar/TopBar.tsx
+++ src/stories/TopBar/TopBar.tsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './TopBar.module.css'
-import { Flex, Text } from '@radix-ui/themes'
+import { Button, Flex, IconButton, Text, TextField } from '@radix-ui/themes'
 import Image from 'next/image'
+import Stepper from '../Stepper/Stepper';
+import { Cross1Icon } from '@radix-ui/react-icons';
 
 export default function TopBar() {
+  const [simulationTitle, setSimulationTitle] = useState("");
+
+  const handleTitleChange = (event: any) => {
+    setSimulationTitle(event.target.value);
+  }
   return (
-    <Flex display="flex" direction="row" py="2" className={styles.barContainer}>
-      <Text mx="2">Nueva simulación - Ciclabilidad</Text>
-      <Image alt="Icono ciclovías" src="/icons/Bicicle.svg" width={25} height={25} />
+    <Flex display="flex" direction="row" py="2" className={styles.barContainer} justify='between'>
+      <Flex>
+        <Flex px="3" ml="2" align="center" justify="between" className={styles.useCaseTag}>
+          <Image alt="Icono ciclovías" src="/icons/Bicicle.svg" width={20} height={20} />
+          <Text mx="1" my="-1" size="2" color='blue' weight='regular'>Ciclabilidad</Text>
+        </Flex>
+        <TextField.Root 
+          mx="4" 
+          placeholder="Nuevo Escenario Sin Título" 
+          className={styles.scenarioTitle}
+          value={simulationTitle}
+          onChange={handleTitleChange}>
+        </TextField.Root>
+      </Flex>
+      <Flex mr="3" gap="2" align="center">
+        <Stepper steps={[{id: 0, name:"Análisis"}, {id: 1, name:"Estrategia"}, {id: 2, name:"Escenario"}]} currentStep={3}/>
+        <IconButton size="3" radius="full" variant="outline">
+          <Cross1Icon width={'65%'} height={'65%'}/>
+        </IconButton>
+      </Flex>
     </Flex>
   )
 }
